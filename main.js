a06eb3fff089e12aa0f6d4e3eef6522a57c9d223
wswon618 <78473609+wswon618@users.noreply.github.com>
2024-10-10 04:15:29 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function switchPlaces() {
+    var startInput = document.getElementById('start');
+    var destinationInput = document.getElementById('destination');
+    var temp = startInput.value;
+    startInput.value = destinationInput.value;
+    destinationInput.value = temp;
+}
+
+function searchPage() {
+    window.location.href = "routefinder.html";
+}
